e568ce5e0ad695b193cf4c39a0ec1853fa40922f
Chandru K <2008chandru.k@gmail.com>
2026-03-18 14:05:32 +0530

    Updated UC7: fixed logic / improved code

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,16 +1,13 @@
 public class HelloApp {
     public static void main(String[] args) {
 
-        // If no arguments → default output
         if (args.length == 0) {
             System.out.println("Hello, World!");
             return;
         }
 
-        // Join all names with ", "
         String joinedNames = String.join(", ", args);
 
-        // Print final output
         System.out.println("Hello, " + joinedNames);
     }
 }
